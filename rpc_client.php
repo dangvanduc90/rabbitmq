596d002b022917c295d4b5eb3441cf4f83f1a804
dangvanduc90 <ducdangvan@vccorp.vn>
2019-01-20 16:23:00 +0700

    remote procedure call

---
+++ rpc_client.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class FibonacciRpcClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            'localhost',
+            5672,
+            'guest',
+            'guest'
+        );
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "",
+            false,
+            false,
+            true,
+            false
+        );
+        $this->channel->basic_consume(
+            $this->callback_queue,
+            '',
+            false,
+            false,
+            false,
+            false,
+            array(
+                $this,
+                'onResponse'
+            )
+        );
+    }
+
+    public function onResponse($rep)
+    {
+        echo 'body: ' . $rep->body. "\n";
+        if ($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n)
+    {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        echo 'callback_queue: ' . $this->callback_queue ."\n";
+        $msg = new AMQPMessage(
+            (string) $n,
+            array(
+                'correlation_id' => $this->corr_id,
+                'reply_to' => $this->callback_queue
+            )
+        );
+        echo 'response2: ' . $this->response . "\n";
+        $this->channel->basic_publish($msg, '', 'rpc_queue');
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        echo 'response3: ' . $this->response . "\n";
+        return intval($this->response);
+    }
+}
+
+$fibonacci_rpc = new FibonacciRpcClient();
+$response = $fibonacci_rpc->call(30);
+echo ' [.] Got ', $response, "\n";
